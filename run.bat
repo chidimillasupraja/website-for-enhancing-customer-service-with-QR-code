f649f40143ca6e54a043145fb6497219f0cfdbb0
chidimillasupraja <chidimillasupraja@gmail.com>
2025-08-06 17:28:29 +0530

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,17 @@
+public class demo{
+     public static void main(String[] args)
+     {
+        int=6,sum=0
+        for(int i=1;i<n;i++)
+        {
+          if(n%i==0)
+              sum=sum+i;
+        }
+        if(sum==n)
+           System.out.println(n+"it is perfect number");
+        else
+            System.out.println(n+"it is not perfect number");
+     }
+ }
+     
+  
